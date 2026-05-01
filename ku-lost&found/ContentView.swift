59aa2340224c17b842c84e4215df12be76d5ce76
Sai Khun Main <saikhunmain.s@ku.th>
2026-05-01 15:32:33 +0700

    Initial Commit

---
+++ ku-lost&found/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ku-lost&found
+//
+//  Created by SK Main on 1/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ku-lost&found/ku_lost_foundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ku_lost_foundApp.swift
+//  ku-lost&found
+//
+//  Created by SK Main on 1/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct ku_lost_foundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
